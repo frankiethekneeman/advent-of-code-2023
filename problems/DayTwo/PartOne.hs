9bb0f5bfc1509036e223b8729266cbfa3d963eec
Francis Sirizzotti <francis@sirizzotti.info>
2023-12-02 02:07:28 -0500

    Day 2

--- problems/DayTwo/PartOne.hs
+++ problems/DayTwo/PartOne.hs
@@ -2,18 +2,75 @@
 module: DayTwo.PartOne
 description: Advent of Code, Day Two, Part One
 -}
-module DayTwo.PartOne(Out, solution) where
+module DayTwo.PartOne(Out, solution, Game(..), Cubes, Color(..), parser) where
 
 import Lib.Solution
 import Lib.Types
 import Helpers.Solution
+import Helpers.Parsing
+import Helpers.Input
+import qualified Data.Map.Strict as Map
+
 
 -- | The type of the answer to this problem
-type Out = Int
+type Out = Integer
 
 examples :: [(String, Out)]
-examples = []
+examples = [ ("one", 1)
+           , ("two", 2)
+           , ("three", 0)
+           , ("four", 0)
+           , ("five", 5)
+           , ("oneThruFive", 8)]
+
+data Color = Red | Green | Blue deriving (Eq, Ord)
+type Cubes = Map.Map Color Integer
+data Game = Game Integer [Cubes]
+
+getId :: Game -> Integer
+getId (Game n _) = n
+
+fromStr :: String -> Result Color
+fromStr "red" = Right Red
+fromStr "blue" = Right Blue
+fromStr "green" = Right Green
+fromStr s = Left $ "Expected known color but got " ++ s
+
+instance ReadableFromToken Color where
+    readTok (StrTok s) = fromStr s
+    readTok _ = Left "Can only parse color from string"
+
+instance Grokkable (Color, Integer) where
+    fromResult = grok2 $ flip (,)
+
+instance Grokkable Cubes where
+    fromResult r = Map.fromList <$> get 0 r
+
+instance Grokkable Game where
+    fromResult = grok2 Game
+
+scanGame :: Scanner
+scanGame = "Game " ^& scanInt ^& ": " ^& (scanReveal ^* "; ") ^& ()
+    where scanReveal = scanCube ^* ", "
+          scanCube = scanInt ^& " " ^& scanColor
+          scanColor = alternating $ map remember ["red", "blue", "green"]
+
+isPossible :: Cubes -> Cubes -> Bool
+isPossible setup candidate = all sufficient [Red, Blue, Green]
+    where sufficient c = Map.findWithDefault 0 c setup >= Map.findWithDefault 0 c candidate
+
+isLegalGame :: Cubes -> Game -> Bool
+isLegalGame setup (Game _ reveals) = all (isPossible setup) reveals
+
+baseSetup :: Cubes
+baseSetup = Map.fromList [(Red, 12), (Green, 13), (Blue, 14)]
+
+sumLegalGames :: [Game] -> Integer
+sumLegalGames = sum . map getId . filter (isLegalGame baseSetup)
+
+parser :: String -> Result [Game]
+parser = lineByLineM $ parse scanGame
 
 -- | Solution for Day Two, Part One
 solution:: AdventProblem Out
-solution = adventOfCode examples (nyi "Parsing" :: String -> Result Out) (nyi "Solution")
+solution = adventOfCode examples parser (always sumLegalGames)
--- problems/DayTwo/PartTwo.hs
+++ problems/DayTwo/PartTwo.hs
@@ -5,15 +5,40 @@ description: Advent of Code, Day Two, Part Two
 module DayTwo.PartTwo(Out, solution) where
 
 import Lib.Solution
-import Lib.Types
+-- import Lib.Types
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 import Helpers.Solution
+import DayTwo.PartOne(parser, Color(..), Game(..), Cubes)
 
 -- | The type of the answer to this problem
-type Out = Int
+type Out = Integer
 
 examples :: [(String, Out)]
-examples = []
+examples = [ ("one", 48)
+           , ("two", 12)
+           , ("three", 1560)
+           , ("four", 630)
+           , ("five", 36)
+           , ("oneThruFive", 2286)]
+
+minOf :: Game -> Color -> Integer
+minOf (Game _ reveals) c = maximum counts
+    where counts = map (Map.findWithDefault 0 c) reveals
+
+colors :: Set.Set Color
+colors = Set.fromList [Red, Blue, Green]
+
+minCubes :: Game -> Cubes
+minCubes g = Map.fromSet (minOf g) colors
+
+power :: Game -> Integer
+power = product . minCubes
+
+combinedGamePowers :: [Game] -> Integer
+combinedGamePowers = sum . map power
 
 -- | Solution for Day Two, Part Two
 solution:: AdventProblem Out
-solution = adventOfCode examples (nyi "Parsing" :: String -> Result Out) (nyi "Solution")
+solution = adventOfCode examples parser (always combinedGamePowers)
+
