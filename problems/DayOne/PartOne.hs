6e3c9059419329b26296e5c14e30eb6f5ac5b12f
Francis Sirizzotti <francis@sirizzotti.info>
2023-12-01 21:25:29 -0500

    Day 1!

--- problems/DayOne/PartOne.hs
+++ problems/DayOne/PartOne.hs
@@ -2,18 +2,36 @@
 module: DayOne.PartOne
 description: Advent of Code, Day One, Part One
 -}
-module DayOne.PartOne(Out, solution) where
+module DayOne.PartOne(Out, solution, sumOfCalibrationValues) where
 
 import Lib.Solution
 import Lib.Types
 import Helpers.Solution
+import Distribution.Simple.Utils
+import Data.Char
 
 -- | The type of the answer to this problem
 type Out = Int
 
 examples :: [(String, Out)]
-examples = []
+examples = [ ("first", 12)
+           , ("second", 38)
+           , ("third", 15)
+           , ("fourth", 77)
+           , ("all", 142)]
+
+calibrationValue :: String -> Result Int
+calibrationValue garbled = maybeToResult "No digits found" number
+    where number = read <$> sequence [tens, units]
+          tens = safeHead digits
+          units = safeLast digits
+          digits = filter isDigit garbled
+
+sumOfCalibrationValues :: [String] -> Result Int
+sumOfCalibrationValues document = sum <$> values
+    where values = sequence $ map calibrationValue document
+
 
 -- | Solution for Day One, Part One
 solution:: AdventProblem Out
-solution = adventOfCode examples (nyi "Parsing" :: String -> Result Out) (nyi "Solution")
+solution = adventOfCode examples (always lines) sumOfCalibrationValues
--- problems/DayOne/PartTwo.hs
+++ problems/DayOne/PartTwo.hs
@@ -5,15 +5,53 @@ description: Advent of Code, Day One, Part Two
 module DayOne.PartTwo(Out, solution) where
 
 import Lib.Solution
-import Lib.Types
 import Helpers.Solution
+import Helpers.Input
+import DayOne.PartOne(sumOfCalibrationValues)
+import GHC.Data.Maybe(orElse)
+import Data.List(stripPrefix)
 
 -- | The type of the answer to this problem
 type Out = Int
 
 examples :: [(String, Out)]
-examples = []
+examples = [ ("first", 12)
+           , ("second", 38)
+           , ("third", 15)
+           , ("fourth", 77)
+           , ("all", 142)
+           , ("fifth", 29)
+           , ("sixth", 83)
+           , ("seventh", 13)
+           , ("eighth", 24)
+           , ("ninth", 42)
+           , ("tenth", 14)
+           , ("eleventh", 76)
+           , ("second_set", 281)
+           , ("overlap", 83)]
+
+prefixes :: [(String, String)]
+prefixes = [("one", "1ne")
+           , ("two", "2wo")
+           , ("three", "3hree")
+           , ("four", "4our")
+           , ("five", "5ive")
+           , ("six", "6ix")
+           , ("seven", "7even")
+           , ("eight", "8ight")
+           , ("nine", "9ine")]
+      
+digitDecode :: String -> String
+digitDecode [] = []
+digitDecode garbled = first:digitDecode rest
+    where first = head replaced
+          rest = tail replaced
+          replaced = foldl (flip . uncurry $ replacePrefix) garbled prefixes
+
+replacePrefix :: String -> String -> String -> String
+replacePrefix prefix replacement str = replaced `orElse` str
+    where replaced = (replacement ++) <$> stripPrefix prefix str
 
 -- | Solution for Day One, Part Two
 solution:: AdventProblem Out
-solution = adventOfCode examples (nyi "Parsing" :: String -> Result Out) (nyi "Solution")
+solution = adventOfCode examples (always $ lineByLine digitDecode) (sumOfCalibrationValues)
