803596f1df6ed57b9893d8679dcbcf5119c2318d
Francis Sirizzotti <francis@sirizzotti.info>
2023-12-08 15:53:50 -0500

    Day 5

--- problems/DayFive/PartOne.hs
+++ problems/DayFive/PartOne.hs
@@ -2,18 +2,87 @@
 module: DayFive.PartOne
 description: Advent of Code, Day Five, Part One
 -}
-module DayFive.PartOne(Out, solution) where
+module DayFive.PartOne(Out, solution, Almanac(..), Lookup(..), LookupMap, parser) where
 
 import Lib.Solution
 import Lib.Types
 import Helpers.Solution
+import Helpers.Parsing
+import qualified Data.Map.Strict as Map
+import GHC.Data.Maybe
 
 -- | The type of the answer to this problem
-type Out = Int
+type Out = Integer
 
 examples :: [(String, Out)]
-examples = []
+examples = [ ("seed79", 82)
+           , ("seed14", 43)
+           , ("seed55", 86)
+           , ("seed13", 35)
+           , ("partOne", 35) ]
+
+type LookupMap = Map.Map Integer (Integer -> Integer)
+
+data Almanac = Almanac [Integer] [Lookup] deriving Show
+data LookupEntry = LookupEntry Integer Integer Integer 
+data Lookup = Lookup String LookupMap LookupMap
+
+instance Show Lookup where
+    show (Lookup name forward backward) = name ++ " (" ++ printEntries forward  ++ ")(" ++ printEntries backward ++")"
+        where printEntries = show . map printEntry . Map.toList 
+              printEntry (start, f) = show start ++ "-> +" ++ (show . f $ 0)
+
+instance Grokkable Almanac where
+    fromResult = grok2 Almanac 
+
+instance Grokkable Lookup where
+    fromResult result = Lookup <$> name <*> forward <*> backward
+        where name = get 0 result
+              forward = toLookup <$> lookups
+              backward = toLookup . map invert <$> lookups
+              lookups = get 1 result
+
+instance Grokkable LookupEntry where
+    fromResult = grok3 LookupEntry
+
+baseLookup :: Map.Map Integer (Maybe Integer)
+baseLookup = Map.singleton 0 Nothing 
+
+invert :: LookupEntry -> LookupEntry
+invert (LookupEntry dest source len) = LookupEntry source dest len
+
+
+toLookup :: [LookupEntry] -> LookupMap
+toLookup = Map.map ((+) . flip orElse 0) . Map.unionsWith firstJust . (baseLookup:) . map mkLookup
+    where mkLookup (LookupEntry dest source len) = Map.fromList [(source, Just $ dest - source), (source + len, Nothing)]
+
+almanacScanner :: Scanner
+almanacScanner = seeds ^& "\n\n" ^& (mapScanner ^* "\n")
+    where seeds = "seeds: " ^& (scanInt ^* " ")
+          mapScanner = mapName ^& " map:\n" ^& (triple ^* "\n")
+          triple = scanInt ^& " " ^& scanInt ^& " " ^& scanInt
+          mapName = alternating $ map remember [ "seed-to-soil"
+                                               , "soil-to-fertilizer"
+                                               , "fertilizer-to-water"
+                                               , "water-to-light"
+                                               , "light-to-temperature"
+                                               , "temperature-to-humidity"
+                                               , "humidity-to-location" ]
+
+parser :: String -> Result Almanac
+parser = parse almanacScanner
+
+solver :: Almanac -> Result Out
+solver (Almanac seeds lookups) = minimum . map doLookups $ seeds
+    where doLookups seed = foldl transition (Right seed) lookups
+
+transition :: Result Integer -> Lookup -> Result Integer
+transition (Left msg) _ = Left msg
+transition (Right num) (Lookup name forward _) = f <*> Right num
+    where f = snd <$> pair
+          pair = maybeToResult (show num ++ " not in " ++ name) $ Map.lookupLE num forward
+          
 
 -- | Solution for Day Five, Part One
 solution:: AdventProblem Out
-solution = adventOfCode examples (nyi "Parsing" :: String -> Result Out) (nyi "Solution")
+solution = adventOfCode examples parser solver
--- problems/DayFive/PartTwo.hs
+++ problems/DayFive/PartTwo.hs
@@ -7,13 +7,80 @@ module DayFive.PartTwo(Out, solution) where
 import Lib.Solution
 import Lib.Types
 import Helpers.Solution
+import DayFive.PartOne(Almanac(..), Lookup(..), LookupMap, parser)
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import Data.Foldable(foldlM)
+import Control.Monad(join)
 
 -- | The type of the answer to this problem
-type Out = Int
+type Out = Integer
 
 examples :: [(String, Out)]
-examples = []
+examples = [("partOne", 46)]
+
+combineLookups :: Lookup -> Lookup -> Result Lookup
+combineLookups (Lookup lName lForward lBackward) (Lookup rName rForward rBackward) = out
+    where out = Lookup name' <$> forward' <*> backward'
+          name' = lName ++ rName
+          forward' = combineMaps lForward rForward lBackward
+          backward'= combineMaps rBackward lBackward rForward
+
+combineMaps :: LookupMap -> LookupMap -> LookupMap -> Result LookupMap
+combineMaps first second backward = monadicMapFromSet combineKey =<< keys
+    where keys = Set.union firstKeys <$> backPropagatedSecondKeys
+          firstKeys = Map.keysSet first
+          backPropagatedSecondKeys = monadicSetMap backPropagate $ Map.keysSet second
+          backPropagate n = getF n backward <*> Right n
+          combineKey k = (.) <$> lhs <*> rhs
+            where rhs = join $ getF <$> rhK <*> Right second
+                  rhK = lhs <*> Right k
+                  lhs = getF k first
+
+monadicSetMap :: (Ord b, Monad m) => (a -> m b) -> Set.Set a -> m (Set.Set b)
+monadicSetMap f set =  Set.fromList <$> mapM f (Set.toList set)
+
+monadicMapFromSet :: (Ord a, Monad m) => (a -> m b) -> Set.Set a -> m (Map.Map a b)
+monadicMapFromSet f set = Map.fromList <$> pairs
+    where pairs = zip keys <$> values
+          values = mapM f keys
+          keys = Set.toList set
+
+
+getF :: Integer -> LookupMap -> Result (Integer -> Integer)
+getF n lookupMap = maybeToResult errMsg $ snd <$> Map.lookupLE n lookupMap
+    where errMsg = "Could not translate " ++ show n ++ " in lookupMap."
+
+baseMap :: LookupMap
+baseMap = Map.singleton 0 (+0)
+
+emptyLookup :: Lookup
+emptyLookup = Lookup "Empty" baseMap baseMap
+
+solver :: Almanac -> Result Out
+solver (Almanac seeds lookups) = minimum <$> candidates
+    where candidates = mapM check =<< pairs
+          check (start, len) = getMin start (start + len - 1) =<< fullLookup
+          pairs = pairsOf seeds
+          fullLookup = foldlM combineLookups emptyLookup lookups
+
+pairsOf :: [a] -> Result [(a, a)]
+pairsOf (l:r:rest) = ((l,r):) <$> pairsOf rest
+pairsOf [] = Right []
+pairsOf _ = Left "List has an uneven number of values"
+
+getMin :: Integer -> Integer -> Lookup -> Result Integer
+getMin start end (Lookup _ forward _) = minimum <$> candidates
+    where candidates = (++) <$> sequence [startFinal, endFinal] <*> restFinal
+          startFinal = translate start
+          endFinal = translate end
+          restFinal = mapM translate $ Map.keys relevantMap
+          relevantMap = fst . Map.split end . snd $ Map.split start forward
+          translate k = getF k forward <*> Right k
+
+          
+
 
 -- | Solution for Day Five, Part Two
 solution:: AdventProblem Out
-solution = adventOfCode examples (nyi "Parsing" :: String -> Result Out) (nyi "Solution")
+solution = adventOfCode examples parser solver
